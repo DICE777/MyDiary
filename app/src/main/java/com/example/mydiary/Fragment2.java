f02067b05e6f11f56135a23d5bb43c76f9e21f50
DICE777 <seulgi7383@gmail.com>
2020-01-25 23:01:56 +0900

    Add input screen

--- app/src/main/java/com/example/mydiary/Fragment2.java
+++ app/src/main/java/com/example/mydiary/Fragment2.java
@@ -1,15 +1,45 @@
 package com.example.mydiary;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.github.channguyen.rsv.RangeSliderView;
+
 public class Fragment2 extends Fragment {
+
+    Context context;
+    OnTabItemSelectedListener listener;
+
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        this.context = context;
+
+        if (context instanceof OnTabItemSelectedListener) {
+            listener = (OnTabItemSelectedListener) context;
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+        if (context != null) {
+            context = null;
+            listener = null;
+        }
+    }
 
     @Nullable
     @Override
@@ -23,5 +53,44 @@ public class Fragment2 extends Fragment {
 
     private void initUI(ViewGroup rootView) {
 
+        Button saveButton = rootView.findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) {
+                    listener.onTabSelected(0);
+                }
+            }
+        });
+
+        Button deleteButton = rootView.findViewById(R.id.deleteButton);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) {
+                    listener.onTabSelected(0);
+                }
+            }
+        });
+
+        Button closeButton = rootView.findViewById(R.id.closeButton);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) {
+                    listener.onTabSelected(0);
+                }
+            }
+        });
+
+        RangeSliderView sliderView = rootView.findViewById(R.id.sliderView);
+        sliderView.setOnSlideListener(new RangeSliderView.OnSlideListener() {
+            @Override
+            public void onSlide(int index) {
+                Toast.makeText(getContext(), "moodIndex Changed to : " + index, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        sliderView.setInitialIndex(2);
     }
 }
