9b8378cd26ecc0d1745ba48c05eb53db85733118
DICE777 <seulgi7383@gmail.com>
2020-01-27 22:55:47 +0900

    Add the pie chart

--- app/src/main/java/com/example/mydiary/Fragment3.java
+++ app/src/main/java/com/example/mydiary/Fragment3.java
@@ -1,5 +1,6 @@
 package com.example.mydiary;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,7 +10,23 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.MPPointF;
+
+import java.util.ArrayList;
+
 public class Fragment3 extends Fragment {
+
+    PieChart chart;
+    BarChart chart2;
+    LineChart chart3;
 
     @Nullable
     @Override
@@ -22,6 +39,57 @@ public class Fragment3 extends Fragment {
     }
 
     private void initUI(ViewGroup rootView) {
+        chart = rootView.findViewById(R.id.chart1);
+        chart.setUsePercentValues(true);
+        chart.getDescription().setEnabled(false);
 
+        chart.setCenterText("기분별 비율");
+
+        chart.setTransparentCircleColor(Color.WHITE);
+        chart.setTransparentCircleAlpha(110);
+        chart.setHoleRadius(58f);
+        chart.setTransparentCircleRadius(61f);
+
+        chart.setDrawCenterText(true);
+
+        chart.setHighlightPerTapEnabled(true);
+
+        Legend legend1 = chart.getLegend();
+        legend1.setEnabled(false);
+
+        chart.setEntryLabelColor(Color.WHITE);
+        chart.setEntryLabelTextSize(12f);
+
+        setData1();
+
+    }
+
+    private void setData1() {
+        ArrayList<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(20.0f, "", getResources().getDrawable(R.drawable.smile1_24)));
+        entries.add(new PieEntry(20.0f, "", getResources().getDrawable(R.drawable.smile2_24)));
+        entries.add(new PieEntry(20.0f, "", getResources().getDrawable(R.drawable.smile3_24)));
+        entries.add(new PieEntry(20.0f, "", getResources().getDrawable(R.drawable.smile4_24)));
+        entries.add(new PieEntry(20.0f, "", getResources().getDrawable(R.drawable.smile5_24)));
+
+        PieDataSet dataSet = new PieDataSet(entries, "기분별 비율");
+
+        dataSet.setDrawIcons(true);
+        dataSet.setSliceSpace(3f);
+        dataSet.setIconsOffset(new MPPointF(0, -40));
+        dataSet.setSelectionShift(5f);
+
+        ArrayList<Integer> colors = new ArrayList<>();
+        for (int c : ColorTemplate.JOYFUL_COLORS) {
+            colors.add(c);
+        }
+        dataSet.setColors(colors);
+
+        PieData data = new PieData(dataSet);
+        data.setValueTextSize(22.0f);
+        data.setValueTextColor(Color.WHITE);
+
+        chart.setData(data);
+        chart.invalidate();
     }
 }
